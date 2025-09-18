176ecddf916496000677a6b1c9634228da98937a
arongeo <me@arongeo.com>
2025-09-18 18:24:02 +0200

    quick comments

--- vision/__init__.py
+++ vision/__init__.py
@@ -1,2 +1,17 @@
+# vision contains all code in relation to the
+# VAE, which is responsible for encoding gameplay
+# frames into a latent space, as well as
+# generating valid frames from the latent vectors
+# 
+# quick file guide:
+# - vae.py:     holds together the encoder and decoder
+#               for the training
+# - encoder.py: the encoder, which generates the 
+#               latent area from a gameplay frame
+#               (mostly unused after training)
+# - decoder.py: the decoder, generates a gameplay frame
+#               from a latent vector
+# - trainer.py: contains the training and sampling functions
+
 from vision.vae import VAE
 from vision.trainer import train_test, sample
