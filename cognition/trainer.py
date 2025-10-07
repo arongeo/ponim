edab898339b16ae7418fa1aecc4b0bf9d9b0b58c
arongeo <me@arongeo.com>
2025-10-07 10:47:38 +0000

    whatthe

--- cognition/trainer.py
+++ cognition/trainer.py
@@ -35,7 +35,7 @@ def train_test(model: Cognition, vae_encoder: Encoder, training_batches, testing
 
                 print(z.shape)
 
-                mco, mu, stdev = model.forward(batch["actions"], torch.cat([torch.zeros(z.shape[0], z.shape[1], z.shape[2], 1), z[:, :, :, :-1]]))
+                mco, mu, stdev = model.forward(batch["actions"], torch.cat([torch.zeros(z.shape[0], z.shape[1], 1), z[:, :, :-1]]))
 
                 loss = Cognition.loss(z, mco, mu, stdev)
 
