7acb13b85376b4fb3b4ccfa3cf03144e509c2489
arongeo <me@arongeo.com>
2025-10-22 07:01:42 +0000

    as

--- run.py
+++ run.py
@@ -51,15 +51,14 @@ while running:
         rmovement = -1.0
     elif keys[pygame.K_DOWN]:
         rmovement = 1.0
-    
+
     rmt = torch.Tensor([[[0.0, rmovement]]]).to(device)
     
     print(rmt)
 
-    mco, lat, stdev = cog.forward(rmt, prev_lat_frames.view(1, 1, -1))
-    most_likely = torch.argmax(mco, dim=-1).squeeze().squeeze().squeeze()
-    prev_lat_frames = torch.cat([lat[:, :, most_likely].clone().detach(), prev_lat_frames[:, :-1]], dim=1)
-    framebuf = vae.decoder.decode(lat[:, :, most_likely]).squeeze().squeeze()
+    lat = cog.forward(rmt, prev_lat_frames.view(1, 1, -1))
+    prev_lat_frames = torch.cat([lat.clone().detach(), prev_lat_frames[:, :-1]], dim=1)
+    framebuf = vae.decoder.decode(lat).squeeze().squeeze()
 
     for i in range(32):
         for j in range(64):
